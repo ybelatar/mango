68d52a09d4c844f8218283b27f8ad0e04b40dda2
Ynsbvv <youncoc@gmail.com>
2024-07-15 17:13:58 +0200

    another change

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
 		return [
 			{
 				// matching all API routes
-				source: "/api.mangadex.org/:path*",
+				source: "https://api.mangadex.org/:path*",
 				headers: [
 					{ key: "Access-Control-Allow-Credentials", value: "true" },
 					{ key: "Access-Control-Allow-Origin", value: "*" },
